cb202ed65aa25c1ac58ccbd614ac561a186ccf1f
DESKTOP-A5M8KDH\derek <derekrichins@gmail.com>
2025-07-06 14:10:19 -0400

    added Select and persist folder URI with SAF and SharedPreferences

--- app/src/main/java/com/example/safandfileprovider/MainActivity.java
+++ app/src/main/java/com/example/safandfileprovider/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.safandfileprovider; // Make sure this matches your project's package name
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,7 +18,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button myExportButton;
+    private static final String PREFS_NAME = "MyPrefsFile";
+    private static final String KEY_SELECTED_URI = "selectedUri";
+    private Button myExportButton;
+    private ActivityResultLauncher<Uri> openDirectoryLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +36,29 @@ public class MainActivity extends AppCompatActivity {
 
         myExportButton = findViewById(R.id.exportButton);
 
+        openDirectoryLauncher = registerForActivityResult(new ActivityResultContracts.OpenDocumentTree(),
+                uri -> {
+                    if (uri != null) {
+
+                        //Getting persistable URI permissions (URI lasts as long as app)
+                        final int takeFlags = Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+                        getContentResolver().takePersistableUriPermission(uri, takeFlags);
+                        //Saving this permission'ed URI to SharedPreferences
+                        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+                        editor.putString(KEY_SELECTED_URI, uri.toString());
+                        editor.apply();
+
+                        Toast.makeText(this, "Folder selection saved",Toast.LENGTH_LONG).show();
+
+                    } else {
+                        Toast.makeText(this, "No folder selected", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
         myExportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+                openDirectoryLauncher.launch(null);
             }
         });
     }
